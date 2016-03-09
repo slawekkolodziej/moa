f00f59afd35f349193dad41f4d098109cbcf2cd7
Slawek Kolodziej <hfrntt@gmail.com>
2016-03-09 17:05:47 +0100

    Add markdown compiler & html template

--- main.go
+++ main.go
@@ -4,7 +4,28 @@ import (
 	"fmt"
 	"gopkg.in/qml.v1"
 	"github.com/russross/blackfriday"
+	"text/template"
+	"bytes"
 )
+
+const htmlDocument = `
+	<!doctype html>
+	<html>
+	<head>
+		<title>{{.Name}}</title>
+	</head>
+	<body>
+		{{.Body}}
+	</body>
+	</html>
+`
+
+type Compiler func(qml.Object, qml.Object)
+
+type Document struct {
+	Name string
+	Body string
+}
 
 func main() {
 	err := qml.Run(runApp)
@@ -25,23 +46,34 @@ func runApp() error {
 	source := win.ObjectByName("source")
 	target := win.ObjectByName("target")
 
+	compile := runCompiler()
+
 	compile(source, target)
-	watch(source, target)
+	watch(source, target, compile)
 
 	win.Show()
 	win.Wait()
 
+
 	return nil
 }
 
-func watch(source qml.Object, target qml.Object) {
+func watch(source qml.Object, target qml.Object, compile Compiler) {
 	source.On("textChanged", func() {
 		compile(source, target)
     })
 }
 
-func compile(source qml.Object, target qml.Object) {
-	input := source.String("text")
-	output := blackfriday.MarkdownBasic([]byte(input))
-	target.Call("loadHtml", string(output))
+func runCompiler() Compiler {
+	htmlDoc := template.Must(template.New("htmlDocument").Parse(`{{.Body}}`))
+
+	return func(source qml.Object, target qml.Object) {
+		var buf bytes.Buffer
+
+		input := source.String("text")
+		output := blackfriday.MarkdownBasic([]byte(input))
+
+		htmlDoc.Execute(&buf, &Document{"test", string(output)})
+		target.Call("loadHtml", buf.String())
+	}
 }
